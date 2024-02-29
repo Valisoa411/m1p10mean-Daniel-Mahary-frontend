7b56e39bfbe913e1746841e6311a3443b663f895
mahariabo123 <andriamahefazafymahali@gmail.com>
2024-02-29 18:21:28 +0300

    update front chart

--- src/app/manager/benefice/benefice.component.ts
+++ src/app/manager/benefice/benefice.component.ts
@@ -37,36 +37,63 @@ export class BeneficeComponent {
     if (this.chartByMonth) {
       this.chartByMonth.destroy();
     }
-
-    this.managerApi.beneficeByMonth(this.selectedYearByMonth).subscribe((data: any[]) => {
-      console.log(data);
-      const labels = data.map(entry => this.getMonthName(entry._id));
-      const counts = data.map(entry => entry.benefice);
-      this.chartByMonth = new Chart('chartByMonth', {
-        type: 'bar',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: 'Benefices par Mois',
-            data: counts,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              type: 'linear', // Assurez-vous que le type d'échelle est correct
-              beginAtZero: true
-            }
-          }
-        }
-      });
-    },
-    (error)=>{
-      alert(error.error.message);
+  
+    // Créer une liste complète des mois de l'année
+    const allMonths = Array.from({ length: 12 }, (_, index) => {
+      const month = index + 1;
+      const formattedMonth = month < 10 ? `0${month}` : month;
+      return `${this.selectedYearByMonth}-${formattedMonth}`;
     });
+  
+    this.managerApi
+      .byMonthChiffreAffaire(this.selectedYearByMonth)
+      .subscribe(
+        (data: any[]) => {
+          const monthDataMap: { [key: string]: number } = {};
+          data.forEach(entry => {
+            monthDataMap[entry._id] = entry.chiffreAffaire;
+          });
+  
+          // Remplir les mois manquants avec un chiffre d'affaires de 0
+          allMonths.forEach(month => {
+            monthDataMap[month] = monthDataMap[month] || 0;
+          });
+  
+          // Trier les données par mois
+          const sortedData = Object.entries(monthDataMap).sort((a, b) => a[0].localeCompare(b[0]));
+  
+          // Extraire les labels et les données triées
+          const labels = sortedData.map(entry => this.getMonthName(entry[0]));
+          const counts = sortedData.map(entry => entry[1]);
+  
+          this.chartByMonth = new Chart('chartByMonth', {
+            type: 'bar',
+            data: {
+              labels: labels,
+              datasets: [
+                {
+                  label: 'Benefices par Mois',
+                  data: counts,
+                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              scales: {
+                y: {
+                  type: 'linear', // Assurez-vous que le type d'échelle est correct
+                  beginAtZero: true,
+                },
+              },
+            },
+          });
+        },
+        (error) => {
+          alert(error.error.message);
+        }
+      );
   }
 
   onYearByMonthChange(): void {
--- src/app/manager/nombrereservation/nombrereservation.component.ts
+++ src/app/manager/nombrereservation/nombrereservation.component.ts
@@ -41,71 +41,132 @@ export class NombrereservationComponent {
       this.chartByDay.destroy();
     }
   }
-
   loadMonth(): void {
     // Détruire le graphique existant avant d'en créer un nouveau
     if (this.chartByMonth) {
       this.chartByMonth.destroy();
     }
-
-    this.managerApi.byMonth(this.selectedYearByMonth).subscribe((data: any[]) => {
-      const labels = data.map(entry => this.getMonthName(entry._id));
-      const counts = data.map(entry => entry.count);
-      this.chartByMonth = new Chart('chartByMonth', {
-        type: 'bar',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: 'Rendez-vous par Mois',
-            data: counts,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              type: 'linear', // Assurez-vous que le type d'échelle est correct
-              beginAtZero: true
-            }
-          }
-        }
-      });
+  
+    // Créer une liste complète des mois de l'année
+    const allMonths = Array.from({ length: 12 }, (_, index) => {
+      const month = index + 1;
+      const formattedMonth = month < 10 ? `0${month}` : month;
+      return `${this.selectedYearByMonth}-${formattedMonth}`;
     });
+  
+    this.managerApi
+      .byMonth(this.selectedYearByMonth)
+      .subscribe(
+        (data: any[]) => {
+          const monthDataMap: { [key: string]: number } = {};
+          data.forEach(entry => {
+            monthDataMap[entry._id] = entry.count;
+          });
+  
+          // Remplir les mois manquants avec un chiffre d'affaires de 0
+          allMonths.forEach(month => {
+            monthDataMap[month] = monthDataMap[month] || 0;
+          });
+  
+          // Trier les données par mois
+          const sortedData = Object.entries(monthDataMap).sort((a, b) => a[0].localeCompare(b[0]));
+  
+          // Extraire les labels et les données triées
+          const labels = sortedData.map(entry => this.getMonthName(entry[0]));
+          const counts = sortedData.map(entry => entry[1]);
+  
+          this.chartByMonth = new Chart('chartByMonth', {
+            type: 'bar',
+            data: {
+              labels: labels,
+              datasets: [
+                {
+                  label: 'Rendez-vous par Mois',
+                  data: counts,
+                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              scales: {
+                y: {
+                  type: 'linear', // Assurez-vous que le type d'échelle est correct
+                  beginAtZero: true,
+                },
+              },
+            },
+          });
+        },
+        (error) => {
+          alert(error.error.message);
+        }
+      );
   }
-
   loadDate(): void {
     // Détruire le graphique existant avant d'en créer un nouveau
     if (this.chartByDay) {
       this.chartByDay.destroy();
     }
-
-    this.managerApi.byDate(this.selectedYearByDate, this.selectedMonth).subscribe((data: any[]) => {
-      const labels = data.map(entry => entry._id);
-      const counts = data.map(entry => entry.count);
-      this.chartByDay = new Chart('chartByDay', {
-        type: 'bar',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: 'Rendez-vous par jour',
-            data: counts,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              type: 'linear', // Assurez-vous que le type d'échelle est correct
-              beginAtZero: true
-            }
-          }
-        }
-      });
+  
+    // Créer une liste complète des jours du mois avec format yyyy-mm-dd
+    const daysInMonth = new Date(this.selectedYearByDate, this.selectedMonth, 0).getDate();
+    const formattedMonth = this.selectedMonth < 10 ? `0${this.selectedMonth}` : this.selectedMonth;
+    const allDays = Array.from({ length: daysInMonth }, (_, index) => {
+      const day = index + 1;
+      const formattedDay = day < 10 ? `0${day}` : day;
+      return `${this.selectedYearByDate}-${formattedMonth}-${formattedDay}`;
     });
+  
+    this.managerApi
+      .byDate(this.selectedYearByDate, this.selectedMonth)
+      .subscribe(
+        (data: any[]) => {
+          console.log(data);
+  
+          // Créer un objet pour stocker les chiffres d'affaires par jour
+          const caByDay: { [key: string]: number } = {};
+          data.forEach(entry => {
+            caByDay[entry._id] = entry.count;
+          });
+  
+          // Remplir les jours manquants avec un chiffre d'affaires de 0
+          allDays.forEach(day => {
+            caByDay[day] = caByDay[day] || 0;
+          });
+  
+          // Extraire les labels et les données triées
+          const sortedData = allDays.map(day => caByDay[day]);
+  
+          this.chartByDay = new Chart('chartByDay', {
+            type: 'bar',
+            data: {
+              labels: allDays,
+              datasets: [
+                {
+                  label: "Rendez-vous par jour",
+                  data: sortedData,
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                  borderColor: 'rgba(255, 99, 132, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              scales: {
+                y: {
+                  type: 'linear', // Assurez-vous que le type d'échelle est correct
+                  beginAtZero: true,
+                },
+              },
+            },
+          });
+        },
+        (error) => {
+          alert(error.error.message);
+        }
+      );
   }
 
   onYearByMonthChange(): void {

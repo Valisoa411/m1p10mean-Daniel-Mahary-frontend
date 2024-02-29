d7d53e625abf702de4a1c114fad54595b9db9a76
mahariabo123 <andriamahefazafymahali@gmail.com>
2024-02-29 22:06:34 +0300

    update env

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,5 +1,6 @@
 // const host = "http://localhost:3000/api"
 const host = "https://m1p11mean-daniel-mahary-backend.vercel.app/api"
+console.log("mety");
 const hostClient = host + "/client"
 const hostEmploye = host + "/employe"
 const hostManager = host + "/manager"
